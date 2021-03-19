bcb2138087c8a8b7723162bd608aca6bfa699f46
Divyaveni Mangalagiri <79857412+Divyaveni@users.noreply.github.com>
2021-03-19 08:44:43 +0530

    Add files via upload

---
+++ 472.5.c
@@ -0,0 +1,40 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    key_t key = ftok("shmfile", 65);
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+    int *num = (int *)shmat(shmid, (void *)0, 0);
+
+    for (int i = 0; i < 5; i++)
+    {
+        int id = fork();
+
+        if (id == 0)
+        {
+            *num = (*num * *num);
+            exit(0);
+        }
+        else
+        {
+            *num = (rand() % 500) + 1;
+            printf("Number Is : %d\n", *num);
+            printf("Memory Address Is : %d\n", num);
+            printf("Size Is : %d\n", sizeof(num));
+            wait(NULL);
+            printf("Square of Number Is : %d\n\n", *num);
+        }
+    }
+
+    shmdt(num);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    fflush(stdin);
+
+    return 0;
+}
